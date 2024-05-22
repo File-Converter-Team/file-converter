1d69b0edf8f39ded521fdab5fb686ef7f0b4bf42
Tytianiuk Artem <tytianiuk@gmail.com>
2024-05-22 23:34:19 +0300

    Implement auth - Create option auth with Google account - Create test design (requires improvement)

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,32 +1,60 @@
-import Link from "next/link";
-import {FileIcon} from "lucide-react";
-import React from "react";
+'use client'
+
+import Link from 'next/link'
+import { FileIcon } from 'lucide-react'
+import React from 'react'
+import { signOut, useSession } from 'next-auth/react'
 
 const Header = () => {
-  return (
-    <header className="w-full bg-gray-900 py-6 px-4 md:px-6">
-      <div className="container mx-auto flex items-center justify-between">
-        <Link className="flex items-center gap-2 text-white" href="/">
-          <FileIcon className="h-6 w-6"/>
-          <span className="text-lg font-semibold">File Converter</span>
-        </Link>
-        <div className="flex items-center space-x-4">
-          <Link
-            className="rounded-md bg-gray-800 px-4 py-2 text-sm font-medium text-white hover:bg-gray-700"
-            href="/auth?tab=signin"
-          >
-            Sign In
-          </Link>
-          <Link
-            className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700"
-            href="/auth?tab=signup"
-          >
-            Sign Up
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-};
+    const session = useSession()
 
-export default Header;
+    console.log(session)
+
+    return (
+        <header className="w-full bg-gray-900 py-6 px-4 md:px-6">
+            <div className="container mx-auto flex items-center justify-between">
+                <Link className="flex items-center gap-2 text-white" href="/">
+                    <FileIcon className="h-6 w-6" />
+                    <span className="text-lg font-semibold">
+                        File Converter
+                    </span>
+                </Link>
+                <div className="flex items-center space-x-4">
+                    {session?.data ? (
+                        <div className="flex items-center gap-2 text-white">
+                            <Link
+                                className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-medium hover:bg-gray-700"
+                                href="#"
+                                onClick={() =>
+                                    console.log(session.data.user!.name)
+                                }
+                            >
+                                {session.data.user!.name}
+                            </Link>
+                            <Link
+                                className="rounded-md bg-red-700 px-4 py-2 text-sm font-medium  hover:bg-gray-700"
+                                href="#"
+                                onClick={() =>
+                                    signOut({
+                                        callbackUrl: '/',
+                                    })
+                                }
+                            >
+                                Sign Out
+                            </Link>
+                        </div>
+                    ) : (
+                        <Link
+                            className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-700"
+                            href="/api/auth/signin"
+                        >
+                            Sign In
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
